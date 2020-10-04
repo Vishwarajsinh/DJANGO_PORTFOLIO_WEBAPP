e0b7bbfa6634d15e1c96094e4c97bdf8a8659ee6
unknown <vishwaraj999@gmail.com>
2020-10-04 21:46:17 +0530

    init commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('services', views.services, name="services"),
+    path('contact', views.contact, name="contact"),
+    path('team', views.team, name="team"),
+
+]
